57df2c2d55e1c559884765cc50bb22d49110d2a7
Artem <swift.uix@gmail.com>
2021-02-20 23:07:48 +0100

    d

--- ActivityApp/ActivityApp/Views/IconsGeometry/SettingsIconView.swift
+++ ActivityApp/ActivityApp/Views/IconsGeometry/SettingsIconView.swift
@@ -10,7 +10,6 @@ import PureSwiftUI
 
 private let startColor = Color.rgb8(228, 229, 231)
 private let endColor = Color.rgb8(139, 143, 150)
-
 private let gradient = LinearGradient([startColor, endColor], angle: .bottom)
 
 private let numTeeth = 54
@@ -19,23 +18,50 @@ private let numSegments = numTeeth * segmentsPerTooth
 
 private let teethLayoutConfig = LayoutGuideConfig.polar(rings: [0.885, 1], segments: numSegments)
 
+private let halfSpokeWidth: CGFloat = 0.025
+private let spokePolarConfig = LayoutGuideConfig.polar(rings: [0.78], segments: 60)
+private let spokeGridConfig = LayoutGuideConfig.grid(columns: [0.5 - halfSpokeWidth, 0.5 + halfSpokeWidth],
+                                                     rows: [0.1, 0.25, 0.46])
+
+private let circleFullDeg = 360
+private let defaultAngle = -30.5.degrees
+
 struct SettingsIconView: View {
     var body: some View {
         let debug = false
         let primaryCogScale: CGFloat = 0.8
         
         ZStack {
-            Shroud()
-                .iconStyle(debug: debug)
-
-            Cog(scale: primaryCogScale)
-                .iconStyle(debug: debug)
-                // .layoutGuide(teethLayoutConfig.scaled(primaryCogScale), color: .green)
+            Color.black.opacity(0.9)
             
-        } // ZSTACK
-        .frame(400)
+            ZStack {
+                Color(white: 0.183)
+                    .clipCircle()
+                    .opacityIf(debug, 0)
+                
+                Shroud()
+                    .iconStyle(debug: debug)
+                
+                Cog(inner: true, scale: 0.53, debug: debug)
+                    .rotateIfNot(debug, defaultAngle)
+                    .iconStyle(debug: debug)
+                
+                Cog(scale: primaryCogScale, debug: debug)
+                    .rotateIfNot(debug, defaultAngle)
+                    .iconStyle(debug: debug)
+//                    .layoutGuide(teethLayoutConfig.scaled(primaryCogScale), color: .green)
+                    .layoutGuide(spokePolarConfig.scaled(primaryCogScale), color: .blue, lineWidth: 1)
+                    .layoutGuide(spokeGridConfig.scaled(primaryCogScale), color: .red, lineWidth: 1)
+                    .layoutGuide(spokeGridConfig.scaled(primaryCogScale).rotated(120.degrees), color: .red, lineWidth: 1)
+            } //: ZSTACK
+            .shadowIfNot(debug, color: .black, radius: 10)
+            .showLayoutGuides(debug)
+            .frame(400)
+        } //: ZSTACK
+        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
     }
 }
+
 
 private extension Shape {
     @ViewBuilder func iconStyle(debug: Bool = false) -> some View {
@@ -49,15 +75,17 @@ private struct Shroud: Shape {
     func path(in rect: CGRect) -> Path {
         var path = Path()
         
-        path.roundedRect(rect, cornerRadius: rect.widthScaled(0.122))
-        path.circle(rect.center, diameter: rect.widthScaled(0.87))
+        path.roundedRect(rect, cornerRadius: rect.sizeScaled(0.122).width)
+        path.circle(rect.center, diameter: rect.sizeScaled(0.87).width)
         
         return path
     }
 }
 
 private struct Cog: Shape {
+    var inner = false
     var scale: CGFloat
+    var debug: Bool
     
     func path(in rect: CGRect) -> Path {
         var path = Path()
@@ -65,17 +93,41 @@ private struct Cog: Shape {
         
         let rotationDelta = pt.angleTo(0, segmentsPerTooth)
         pt = pt.rotated(-pt.angleTo(1, 5))
+        
         path.move(pt[0, 0])
         
         for tooth in 0..<numTeeth {
             let localPt = pt.rotated(rotationDelta * tooth)
-            
             path.line(localPt[1, 4])
             path.line(localPt[1, 6])
-            path.line(localPt[0, segmentsPerTooth / 2]) // 10
-            path.line(localPt[0, segmentsPerTooth]) // 20
+            path.line(localPt[0, segmentsPerTooth / 2])
+            path.line(localPt[0, segmentsPerTooth])
         }
+        
+        path.closeSubpath()
 
+        if !inner {
+            var p = spokePolarConfig.scaled(scale).layout(in: rect)
+            var g = spokeGridConfig.scaled(scale).layout(in: rect)
+            
+            for _ in 0..<3 {
+                path.move(g[1, 2])
+                path.line(g[1, 1])
+                path.curve(p[0, 3], cp1: g[1, 0], cp2: p[0, 2], showControlPoints: debug)
+                path.arc(rect.center, radius: p.radiusTo(0, 0), startAngle: p.angleTo(0, 3), endAngle: p.angleTo(0, 17))
+                
+                g = g.rotated(120.degrees)
+                path.curve(g[0, 1], cp1: p[0, 18], cp2: g[0, 0], showControlPoints: debug)
+                path.line(g[0, 2])
+                path.closeSubpath()
+                
+                p = p.rotated((circleFullDeg / 3).degrees)
+            }
+            path.circle(rect.center, diameter: rect.widthScaled(0.04 * scale))
+        } else {
+            path.circle(rect.center, diameter: rect.widthScaled(0.72 * scale))
+        }
+        
         return path
     }
 }
@@ -83,8 +135,5 @@ private struct Cog: Shape {
 struct SettingsIconView_Previews: PreviewProvider {
     static var previews: some View {
         SettingsIconView()
-            .greedyFrame()
-            .background(Color.black.opacity(0.9).ignoresSafeArea())
-            .showLayoutGuides(true)
     }
 }
