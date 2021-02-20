66931988e7af84cd816f1e322435558855a5db21
Artem <swift.uix@gmail.com>
2021-02-20 13:16:16 +0100

    d

---
+++ ActivityApp/ActivityApp/Views/CoreConcepts/GeometryReader.swift
@@ -0,0 +1,20 @@
+//
+//  GeometryReader.swift
+//  ActivityApp
+//
+//  Created by Артём Мошнин on 20/2/21.
+//
+
+import SwiftUI
+
+struct GeometryReader: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GeometryReader_Previews: PreviewProvider {
+    static var previews: some View {
+        GeometryReader()
+    }
+}
