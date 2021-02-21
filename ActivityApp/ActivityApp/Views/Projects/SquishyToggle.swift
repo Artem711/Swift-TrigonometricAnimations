a49c13d19dea14f027275f22fec7e07456c0e340
Artem <swift.uix@gmail.com>
2021-02-21 12:04:22 +0100

    toggle shape

--- ActivityApp/ActivityApp/Views/Projects/SquishyToggle.swift
+++ ActivityApp/ActivityApp/Views/Projects/SquishyToggle.swift
@@ -11,20 +11,24 @@ import PureSwiftUI
 // MARK: - Constants
 
 // MARK: - Layout Guides
+private let frameLayoutConfig = LayoutGuideConfig.grid(columns: [0.25, 0.4, 0.6, 0.75], rows: 2)
 
 struct SquishyToggle: View {
     var body: some View {
         let debug = true
+        
         GeometryReader(content: { geometry in
             let size = calculateSize(from: geometry)
             ZStack {
                 ToggleFrame(debug: debug)
                     .styling(color: .green)
+                    .layoutGuide(frameLayoutConfig, color: .green, lineWidth: 2)
             } //: ZSTACK
             .frame(size) // .frame(width: size.width, height: size.height)
             .borderIf(debug, Color.gray.opacity(0.2))
             .greedyFrame() // .frame(width: .infinity, height: .infinity)
-        })
+        }) //: GEOMETRY_READER
+        .showLayoutGuides(true)
     }
     
     // MARK: - Calculate size of the shape (we want (WIDTH) = (2 x HEIGHT))
@@ -47,6 +51,18 @@ private struct ToggleFrame: Shape {
     
     func path(in rect: CGRect) -> Path {
         var path = Path()
+        var g = frameLayoutConfig.layout(in: rect)
+        
+        let curveYOffset = rect.heightScaled(0.18)
+        let arcRadius = rect.height / 2
+        path.move(g[0, 0])
+        path.curve(rect.top.yOffset(curveYOffset), cp1: g[1, 0], cp2: g[1, 0].yOffset(curveYOffset), showControlPoints: debug)
+        path.curve(g[3, 0], cp1: g[2, 0].yOffset(curveYOffset), cp2: g[2, 0], showControlPoints: debug)
+        path.arc(g[3, 1], radius: arcRadius, startAngle: .top, endAngle: .bottom)
+        path.curve(rect.bottom.yOffset(-curveYOffset), cp1: g[2, 2], cp2: g[2, 2].yOffset(-curveYOffset), showControlPoints: debug)
+        path.curve(g[0, 2], cp1: g[1, 2].yOffset(-curveYOffset), cp2: g[1, 2], showControlPoints: debug)
+        path.arc(g[0, 1], radius: arcRadius, startAngle: .bottom, endAngle: .top)
+        
         return path
     }
     
@@ -65,3 +81,33 @@ struct SquishyToggle_Previews: PreviewProvider {
             .frame(400)
     }
 }
+//
+//let curveYOffset = rect.heightScaled(0.18)
+//let arcRadius = rect.halfHeight
+//path.move(g[0, 0])
+//path.curve(
+//    rect.top.yOffset(curveYOffset),
+//    cp1: g[1, 0],
+//    cp2: g[1, 0].yOffset(curveYOffset),
+//    showControlPoints: debug)
+//
+//path.curve(
+//    g[3, 0],
+//    cp1: g[2, 0].yOffset(curveYOffset),
+//    cp2: g[2, 0],
+//    showControlPoints: true)
+//
+//path.arc(g[3, 1], radius: arcRadius, startAngle: .top, endAngle: .bottom)
+//path.curve(
+//    rect.bottom.yOffset(-curveYOffset),
+//    cp1: g[2, 2],
+//    cp2: g[2, 2].yOffset(-curveYOffset),
+//    showControlPoints: true)
+//
+//path.curve(
+//    g[0, 2],
+//    cp1: g[1, 2].yOffset(-curveYOffset),
+//    cp2: g[1, 2],
+//    showControlPoints: true)
+//
+//path.arc(g[0, 1], radius: arcRadius, startAngle: .bottom, endAngle: .top)
